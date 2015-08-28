648c6397e913f2f4acb58404e72a9985b872326c
loire <etienneloire@gmail.com>
2015-08-28 17:07:07 +0200

    change path of tmp

--- bac2ass.sh
+++ bac2ass.sh
@@ -56,16 +56,16 @@ mkdir -p mapVectorCont
 echo "Filtering of Vector and contaminents in reads"
 
 echo ./indexVector_Cont/$(basename $3)
-/home/bin/bwa/last/x64/bwa mem -M -t 8 ./indexVector_Cont/$(basename $3)  ./tmp/${base}_q$4_R1.fastq > ./mapVectorCont/tmp_1.sam
-samtools view -bS ./mapVectorCont/tmp_1.sam > ./mapVectorCont/tmp_1.bam
-samtools sort -n ./mapVectorCont/tmp_1.bam ./mapVectorCont/tmp_1.sorted
-samtools view ./mapVectorCont/tmp_1.sorted.bam >  ./mapVectorCont/${base}.paired_1.sam 
-rm ./mapVectorCont/tmp_1.*
-/home/bin/bwa/last/x64/bwa mem -M -t 8 ./indexVector_Cont/$(basename $3)  ./tmp/${base}_q$4_R2.fastq > ./mapVectorCont/tmp_2.sam
-samtools view -bS ./mapVectorCont/tmp_2.sam > ./mapVectorCont/tmp_2.bam
-samtools sort -n ./mapVectorCont/tmp_2.bam ./mapVectorCont/tmp_2.sorted
-samtools view ./mapVectorCont/tmp_2.sorted.bam >  ./mapVectorCont/${base}.paired_2.sam 
-rm ./mapVectorCont/tmp_2.*
+/home/bin/bwa/last/x64/bwa mem -M -t 8 ./indexVector_Cont/$(basename $3)  ./tmp/${base}_q$4_R1.fastq > ./mapVectorCont/${base}_tmp_1.sam
+samtools view -bS ./mapVectorCont/${base}_tmp_1.sam > ./mapVectorCont/${base}_tmp_1.bam
+samtools sort -n ./mapVectorCont/${base}_tmp_1.bam ./mapVectorCont/${base}_tmp_1.sorted
+samtools view ./mapVectorCont/${base}_tmp_1.sorted.bam >  ./mapVectorCont/${base}.paired_1.sam 
+rm ./mapVectorCont/${base}_tmp_1.*
+/home/bin/bwa/last/x64/bwa mem -M -t 8 ./indexVector_Cont/$(basename $3)  ./tmp/${base}_q$4_R2.fastq > ./mapVectorCont/${base}_tmp_2.sam
+samtools view -bS ./mapVectorCont/${base}_tmp_2.sam > ./mapVectorCont/${base}_tmp_2.bam
+samtools sort -n ./mapVectorCont/${base}_tmp_2.bam ./mapVectorCont/${base}_tmp_2.sorted
+samtools view ./mapVectorCont/${base}_tmp_2.sorted.bam >  ./mapVectorCont/${base}.paired_2.sam 
+rm ./mapVectorCont/${base}_tmp_2.*
 #
 ## Same with frag reads produced by flash, but without the useless sorting.
 #
